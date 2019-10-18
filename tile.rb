04f6a70b89026d627b62f0ec11cac7de2ae318ca
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-10-18 23:05:28 +0200

    Add method checking neighbours bomb and colorize UI

--- tile.rb
+++ tile.rb
@@ -1,21 +1,29 @@
+require "colorize"
+
 class Tile
   attr_reader :bombed, :revealed
 
-  def initialize()
+  def initialize(board)
+    @board = board
     @bombed = false
     @flagged = false
     @revealed = false
+    @pos = nil
+    @bomb_count = 0
   end
 
   #   rendering board
   def to_s
     return "*" if @revealed == false
 
-    neighbor_bomb_count ? neighbor_bomb_count : "_"
+    # debugger
+    neighbor_bomb_count ? "#{neighbor_bomb_count}" : "_".colorize(:green)
   end
 
-  def reveal
+  def reveal(pos)
+    @pos = pos
     return false if @bombed
+
     @revealed = true
 
     true
@@ -26,17 +34,41 @@ class Tile
   end
 
   def neighbor_bomb_count
+    return @bomb_count if @bomb_count > 0
+
+    x, y = @pos
+    direct_neighbors = [
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1],
+
+      [x - 1, y],
+      [x + 1, y],
+
+      [x - 1, y - 1],
+      [x, y - 1],
+      [x + 1, y - 1],
+    ]
+
+    direct_neighbors.each do |neighbor|
+      x, y = neighbor
+      if @board[x][y] && @board[x][y].bombed
+        @bomb_count += 1
+      end
+    end
+
+    return @bomb_count if @bomb_count > 0
+
     false
-    # filter neighbour with bombs
   end
 
   def set_bomb
     @bombed = true
   end
 
-  def act_on_tile(action)
+  def act_on_tile(action, pos)
     if action == "r"
-      reveal
+      reveal(pos)
     else
       @flagged = !@flagged
     end
