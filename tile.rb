6da51e14b86946feacf02b8267124ce8782d1dc7
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-10-20 00:38:28 +0200

    Adds revealing neighbour safe tiles

--- tile.rb
+++ tile.rb
@@ -17,41 +17,52 @@ class Tile
   #   rendering board
   def to_s
     return "*" if @revealed == false
+
     return "!".colorize(:red) if @bombed == true
 
-    neighbor_bomb_count ? "#{neighbor_bomb_count}" : "_".colorize(:green)
+    return "#{@bomb_count}".colorize(:yellow) if @bomb_count.positive?
+
+    "_".colorize(:green)
   end
 
   def reveal
-    return false if @bombed
-
     @revealed = true
+
+    neighbor_bomb_count
+
+    if @bomb_count.zero?
+      reveal_neighbors
+    end
+
+    return false if @bombed
 
     true
   end
 
-  def neighbors
-    # BFS?
+  def revealed?
+    revealed
+  end
+
+  def reveal_neighbors
+    @direct_neighbors.each do |neighbor|
+      x, y = neighbor
+      if !@board[x][y].revealed?
+        @board[x][y].reveal
+      end
+    end
   end
 
   def neighbor_bomb_count
-    return @bomb_count if @bomb_count > 0
-
-    # @direct_neighbors = @direct_neighbors ||  direct_neighbors(board)
     @direct_neighbors ||= direct_neighbors
 
     direct_neighbors.each do |neighbor|
       x, y = neighbor
-      # debugger if @board[x][y] == nil
       if @board[x] && @board[x][y] && @board[x][y].bombed
-        # debugger
         @bomb_count += 1
       end
     end
 
-    return @bomb_count if @bomb_count > 0
-
-    false
+    @bomb_count
   end
 
   def set_bomb
@@ -67,7 +78,6 @@ class Tile
   end
 
   def direct_neighbors
-    # debugger
     @direct_neighbors = []
 
     x, y = @pos
@@ -86,8 +96,6 @@ class Tile
 
     possible_neighbors.each do |neighbor|
       x, y = neighbor
-      # debugger if @board[x][y].nil?
-      # debugger if x == 0 && y < 0
       if x >= 0 && y >= 0 && @board[x] && @board[x][y]
         @direct_neighbors << neighbor
       end
